b58eb31210f6ef66b361c1e3818433ca2f5e1f40
Gabriel Pacheco <cgabrielpacheco97@gmail.com>
2023-10-23 13:25:18 -0300

    path * added

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/events', require('./routes/events.routes'))
 
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
 
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
